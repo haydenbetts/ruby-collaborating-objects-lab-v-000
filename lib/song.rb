d6c06e7b66cccc64fee08059d48615048761e401
Hayden Betts <haydenbetts@gmail.com>
2018-06-28 18:43:43 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -7,7 +7,7 @@ class Song
   end
 
   def self.new_by_filename(filename)
-    filename_no_extension = filename.match(/(.+).mp3/).captures.to_s
+    filename_no_extension = filename.match(/(.+).mp3/).captures[0]
     artist_name, song_title, genre = filename_no_extension.split(/\s-\s/)
     binding.pry
   end
